d8b4d82fdcb41d269151bf06103484508a6bb279
Matthew Weiper <mattweiper@gmail.com>
2021-05-28 12:11:41 -0400

    Changed URLs so Flask will work properly

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
+        f"Available Routes:"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
         f"/api/v1.0/<start>/<end>"
     )
 @app.route("/api/v1.0/precipitation")
